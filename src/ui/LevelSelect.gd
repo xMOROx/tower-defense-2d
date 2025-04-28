2549518d755038852462a19ecd84908cef630cf5
krisx303 <57154118+krisx303@users.noreply.github.com>
2025-04-28 12:41:15 +0200

    Displaying total stars

--- src/ui/LevelSelect.gd
+++ src/ui/LevelSelect.gd
@@ -2,17 +2,19 @@ extends Control
 
 @onready var level_1_button: Button = $Levels/Row1/Level1
 @onready var menu_button: Button = $MenuButton
+@onready var score_label: Label = $Score/ScoreLabel
 
 var level_1_scene_path = "res://scenes/maps/Level_1.tscn"
 var stars0 = load("res://assets/ui/Level/Star/Group/0-3.png")
 var stars1 = load("res://assets/ui/Level/Star/Group/1-3.png")
 var stars2 = load("res://assets/ui/Level/Star/Group/2-3.png")
 var stars3 = load("res://assets/ui/Level/Star/Group/3-3.png")
+var levels = [level_1_scene_path]
 
 func _ready():
 	level_1_button.pressed.connect(_on_level_1_button_pressed)
 	menu_button.pressed.connect(_on_back_button_pressed)
-	_update_level_stars(level_1_button, level_1_scene_path)
+	_on_game_progress_loaded()
 
 func _on_level_1_button_pressed():
 	print("LevelSelect: Level 1 selected")
@@ -24,7 +26,12 @@ func _on_back_button_pressed():
 
 func _on_game_progress_loaded():
 	print("LevelSelect: Game progress loaded")
+	var all_obtained_stars: int = 0
+	for level in levels:
+		all_obtained_stars += StateManager.get_level_stars(level)
+	score_label.text = str(all_obtained_stars) + "/" + str(levels.size()*3)
 	_update_level_stars(level_1_button, level_1_scene_path)
+	
 	
 func _update_level_stars(button: Button, level_path: String):	
 	print("LevelSelect: Updating stars for level", level_path)
