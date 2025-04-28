aa4e0824bc089cc21da785bb9c718bad94669510
Krzysztof Usnarski <57154118+krisx303@users.noreply.github.com>
2025-04-28 07:31:38 +0200

    Added PauseMenu (#31)

---
+++ src/ui/PauseMenu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var resume_button: Button = $Background/Buttons/ResumeButton
+@onready var settings_button: Button = $Background/Buttons/SettingsButton
+@onready var main_menu_button: Button = $Background/Buttons/MainMenuButton
+
+func _ready():
+	resume_button.pressed.connect(_on_resume_button_pressed)
+	settings_button.pressed.connect(_on_settings_button_pressed)
+	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
+
+func _on_resume_button_pressed():
+	SceneManager.toggle_pause_menu()
+
+func _on_main_menu_button_pressed():
+	SceneManager.goto_main_menu()
+	
+func _on_settings_button_pressed():	
+	print("PauseMenu: Settings button pressed")
